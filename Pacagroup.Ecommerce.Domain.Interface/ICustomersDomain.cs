6b0ce7f8bae1e05bcc4b58fce0e56dc5dea2072f
AndresFerreiraR <ac.ferreira.r@gmail.com>
2024-01-23 17:22:27 -0500

    Se genera los contratos y las implementaciones para la capa dominio

---
+++ Pacagroup.Ecommerce.Domain.Interface/ICustomersDomain.cs
@@ -0,0 +1,31 @@
+﻿using Pacagroup.Ecommerce.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pacagroup.Ecommerce.Domain.Interface
+{
+    public interface ICustomersDomain
+    {
+        #region Métodos Sincronos
+
+        bool Insert(Customers customers);
+        bool Update(Customers customers);
+        bool Delete(string customerId);
+        Customers Get(string customerId);
+        IEnumerable<Customers> GetAll();
+
+        #endregion
+
+        #region Métodos Asíncronos
+
+        Task<bool> InsertAsync(Customers customers);
+        Task<bool> UpdateAsync(Customers customers);
+        Task<bool> DeleteAsync(string customerId);
+        Task<Customers> GetAsync(string customerId);
+        Task<IEnumerable<Customers>> GetAllAsync();
+
+        #endregion
+    }
+}
