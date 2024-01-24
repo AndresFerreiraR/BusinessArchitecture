5a86b49ba0bfd8cc59dafc57f8d502faeb60bb65
AndresFerreiraR <ac.ferreira.r@gmail.com>
2024-01-24 16:28:24 -0500

    Se crea la capa de aplicacion o negocio y se implementa el mapeo de entidades a DTOs

---
+++ Pacagroup.Ecommerce.Transversal.Mapper/MappingsProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Pacagroup.Ecommerce.Application.DTO;
+using Pacagroup.Ecommerce.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pacagroup.Ecommerce.Transversal.Mapper
+{
+    public class MappingsProfile : Profile
+    {
+        public MappingsProfile()
+        {
+            CreateMap<Customers, CustomersDto>().ReverseMap();
+        }
+    }
+}
